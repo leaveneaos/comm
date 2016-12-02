194c3277cda9fb924e10a8a463a837d9b923dca4
kangzhongxu <kangzhongxu123>
2016-12-02 11:19:32 +0800

    新增xml转object、object转xml样式工具类

---
+++ src/main/java/com/rjxx/test/COMMON_FPKJ_FPT.java
@@ -0,0 +1,233 @@
+package com.rjxx.test;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+public class COMMON_FPKJ_FPT {
+	
+	private String FPQQLSH;
+	
+	private String KPLX;
+	
+	private String BMB_BBH;
+
+    private String ZSFS;
+	
+	private String XSF_NSRSBH;
+	
+	private String XSF_MC;
+
+    private String XSF_DZDH;
+	
+	private String XSF_YHZH;
+
+      
+    private String GMF_NSRSBH;
+	
+	private String GMF_MC;
+	
+	private String GMF_DZDH;
+
+	private String GMF_YHZH;
+	
+	private String KPR;
+
+	private String SKR;
+
+	private String FHR;
+
+	private String YFP_DM;
+
+	private String YFP_HM;
+
+	private String JSHJ;
+
+	private String HJJE;
+
+	private String HJSE;
+    
+	private String KCE;
+    
+	private String BZ;
+
+	public String getFPQQLSH() {
+		return FPQQLSH;
+	}
+
+	public void setFPQQLSH(String fPQQLSH) {
+		FPQQLSH = fPQQLSH;
+	}
+
+	public String getKPLX() {
+		return KPLX;
+	}
+
+	public void setKPLX(String kPLX) {
+		KPLX = kPLX;
+	}
+
+	public String getBMB_BBH() {
+		return BMB_BBH;
+	}
+
+	public void setBMB_BBH(String bMB_BBH) {
+		BMB_BBH = bMB_BBH;
+	}
+
+	public String getZSFS() {
+		return ZSFS;
+	}
+
+	public void setZSFS(String zSFS) {
+		ZSFS = zSFS;
+	}
+
+	public String getXSF_NSRSBH() {
+		return XSF_NSRSBH;
+	}
+
+	public void setXSF_NSRSBH(String xSF_NSRSBH) {
+		XSF_NSRSBH = xSF_NSRSBH;
+	}
+
+	public String getXSF_MC() {
+		return XSF_MC;
+	}
+
+	public void setXSF_MC(String xSF_MC) {
+		XSF_MC = xSF_MC;
+	}
+
+	public String getXSF_DZDH() {
+		return XSF_DZDH;
+	}
+
+	public void setXSF_DZDH(String xSF_DZDH) {
+		XSF_DZDH = xSF_DZDH;
+	}
+
+	public String getXSF_YHZH() {
+		return XSF_YHZH;
+	}
+
+	public void setXSF_YHZH(String xSF_YHZH) {
+		XSF_YHZH = xSF_YHZH;
+	}
+
+	public String getGMF_NSRSBH() {
+		return GMF_NSRSBH;
+	}
+
+	public void setGMF_NSRSBH(String gMF_NSRSBH) {
+		GMF_NSRSBH = gMF_NSRSBH;
+	}
+
+	public String getGMF_MC() {
+		return GMF_MC;
+	}
+
+	public void setGMF_MC(String gMF_MC) {
+		GMF_MC = gMF_MC;
+	}
+
+	public String getGMF_DZDH() {
+		return GMF_DZDH;
+	}
+
+	public void setGMF_DZDH(String gMF_DZDH) {
+		GMF_DZDH = gMF_DZDH;
+	}
+
+	public String getGMF_YHZH() {
+		return GMF_YHZH;
+	}
+
+	public void setGMF_YHZH(String gMF_YHZH) {
+		GMF_YHZH = gMF_YHZH;
+	}
+
+	public String getKPR() {
+		return KPR;
+	}
+
+	public void setKPR(String kPR) {
+		KPR = kPR;
+	}
+
+	public String getSKR() {
+		return SKR;
+	}
+
+	public void setSKR(String sKR) {
+		SKR = sKR;
+	}
+
+	public String getFHR() {
+		return FHR;
+	}
+
+	public void setFHR(String fHR) {
+		FHR = fHR;
+	}
+
+	public String getYFP_DM() {
+		return YFP_DM;
+	}
+
+	public void setYFP_DM(String yFP_DM) {
+		YFP_DM = yFP_DM;
+	}
+
+	public String getYFP_HM() {
+		return YFP_HM;
+	}
+
+	public void setYFP_HM(String yFP_HM) {
+		YFP_HM = yFP_HM;
+	}
+
+	public String getJSHJ() {
+		return JSHJ;
+	}
+
+	public void setJSHJ(String jSHJ) {
+		JSHJ = jSHJ;
+	}
+
+	public String getHJJE() {
+		return HJJE;
+	}
+
+	public void setHJJE(String hJJE) {
+		HJJE = hJJE;
+	}
+
+	public String getHJSE() {
+		return HJSE;
+	}
+
+	public void setHJSE(String hJSE) {
+		HJSE = hJSE;
+	}
+
+	public String getKCE() {
+		return KCE;
+	}
+
+	public void setKCE(String kCE) {
+		KCE = kCE;
+	}
+
+	public String getBZ() {
+		return BZ;
+	}
+
+	public void setBZ(String bZ) {
+		BZ = bZ;
+	}
+	
+
+
+}
---
+++ src/main/java/com/rjxx/test/COMMON_FPKJ_XMXX.java
@@ -0,0 +1,108 @@
+package com.rjxx.test;
+
+
+public class COMMON_FPKJ_XMXX {
+
+    private String FPHXZ;
+	private String SPBM;
+	private String ZXBM;
+	private String YHZCBS;
+	private String LSLBS;
+	private String ZZSTSGL;
+	private String XMMC;
+	private String GGXH;
+	private String DW;
+	private String XMSL;
+	private String XMDJ;
+	private String XMJE;
+	private String SL;
+	private String SE;
+	public String getFPHXZ() {
+		return FPHXZ;
+	}
+	public void setFPHXZ(String fPHXZ) {
+		FPHXZ = fPHXZ;
+	}
+	public String getSPBM() {
+		return SPBM;
+	}
+	public void setSPBM(String sPBM) {
+		SPBM = sPBM;
+	}
+	public String getZXBM() {
+		return ZXBM;
+	}
+	public void setZXBM(String zXBM) {
+		ZXBM = zXBM;
+	}
+	public String getYHZCBS() {
+		return YHZCBS;
+	}
+	public void setYHZCBS(String yHZCBS) {
+		YHZCBS = yHZCBS;
+	}
+	public String getLSLBS() {
+		return LSLBS;
+	}
+	public void setLSLBS(String lSLBS) {
+		LSLBS = lSLBS;
+	}
+	public String getZZSTSGL() {
+		return ZZSTSGL;
+	}
+	public void setZZSTSGL(String zZSTSGL) {
+		ZZSTSGL = zZSTSGL;
+	}
+	public String getXMMC() {
+		return XMMC;
+	}
+	public void setXMMC(String xMMC) {
+		XMMC = xMMC;
+	}
+	public String getGGXH() {
+		return GGXH;
+	}
+	public void setGGXH(String gGXH) {
+		GGXH = gGXH;
+	}
+	public String getDW() {
+		return DW;
+	}
+	public void setDW(String dW) {
+		DW = dW;
+	}
+	public String getXMSL() {
+		return XMSL;
+	}
+	public void setXMSL(String xMSL) {
+		XMSL = xMSL;
+	}
+	public String getXMDJ() {
+		return XMDJ;
+	}
+	public void setXMDJ(String xMDJ) {
+		XMDJ = xMDJ;
+	}
+	public String getXMJE() {
+		return XMJE;
+	}
+	public void setXMJE(String xMJE) {
+		XMJE = xMJE;
+	}
+	public String getSL() {
+		return SL;
+	}
+	public void setSL(String sL) {
+		SL = sL;
+	}
+	public String getSE() {
+		return SE;
+	}
+	public void setSE(String sE) {
+		SE = sE;
+	}
+	
+	
+	
+	
+}
