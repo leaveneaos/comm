e41c26b68efe4ffc45c2fb59a5cd23ab4746398b
liuyan@datarj.com <liuyan@datarj.com>
2016-12-14 14:59:57 +0800

    修改发送邮件接口返回值

--- src/main/java/com/rjxx/utils/SendMessage.java
+++ src/main/java/com/rjxx/utils/SendMessage.java
@@ -16,9 +16,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class SendMessage {
 
-	public static boolean sendMessage(String qmmc, String mbCode, Map<String, String> content, String sjhm) {
+	public static String sendMessage(String qmmc, String mbCode, Map<String, String> content, String sjhm) {
 		IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", "5T6XUKr6uxSfhNAu","a7cBFQR3avT4NSIR6dFtP8GLvzcL5G"
 				);
+		String message="";
 		try {
 			DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", "Sms", "sms.aliyuncs.com");
 			IAcsClient client = new DefaultAcsClient(profile);
@@ -30,17 +31,16 @@ public class SendMessage {
 			request.setParamString(json);
 			request.setRecNum(sjhm);
 			SingleSendSmsResponse httpResponse = client.getAcsResponse(request);
+			message = httpResponse.getRequestId();
+			return message;
 		} catch (ServerException e) {
 			e.printStackTrace();
-			return false;
 		} catch (ClientException e) {
 			e.printStackTrace();
-			return false;
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
-			return false;
 		}
-		return true;
+		return message;
 	}
 
 	public static void main(String[] args) {
