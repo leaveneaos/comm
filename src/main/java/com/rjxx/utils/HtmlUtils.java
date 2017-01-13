741238e76afc0312714338ffa3971b7ebffd8bb8
张兵 <zhangbing@datarj.com>
2017-01-13 15:39:58 +0800

    获取项目路径方法

--- src/main/java/com/rjxx/utils/HtmlUtils.java
+++ src/main/java/com/rjxx/utils/HtmlUtils.java
@@ -1,5 +1,7 @@
 package com.rjxx.utils;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by Administrator on 2016/9/23.
  */
@@ -13,6 +15,23 @@ public class HtmlUtils {
      */
     public static String ignoreHtmlTag(String htmlContent) {
         return htmlContent.replaceAll("<(!|/)?(.|\\n)*?>", "").replaceAll("&nbsp;", "");
+    }
+
+    /**
+     * 获取项目路径
+     *
+     * @param request
+     * @return
+     */
+    public static String getBasePath(HttpServletRequest request) {
+        String path = request.getContextPath();
+        String basePath = null;
+        if (request.getServerPort() == 80 || request.getServerPort() == 443) {
+            basePath = request.getScheme() + "://" + request.getServerName() + path + "/";
+        } else {
+            basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + path + "/";
+        }
+        return basePath;
     }
 
 }
